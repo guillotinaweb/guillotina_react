92401ad1b3a1a80f2c3aa103073887e51bf0a90f
Jordi Collell <jordic@gmail.com>
2019-12-17 17:37:34 +0100

    Allow exact search with ""

--- src/guillo-gmi/lib/search.js
+++ src/guillo-gmi/lib/search.js
@@ -10,13 +10,22 @@ text=asdf nombre=xxx ->
 
 const SEP="="
 const DEFAULT_FIELD = "title__in"
+const CLEANER = "||"
 
 
 export function parser(qs, defaultField=DEFAULT_FIELD) {
   let lastKey = undefined
   qs.trim()
+
+  if (qs.includes('"')) {
+    qs = qs.replace(/"(\w+) (\w+)"/,`$1${CLEANER}$2`)
+  }
+
   qs = qs.split(" ")
   return qs.map(part => {
+    if (part.includes(CLEANER)) {
+      part = part.replace("||", " ")
+    }
     if (!part.includes(SEP)) {
         return !lastKey ? [defaultField, part] : [lastKey, part]
     }
