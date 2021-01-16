1668d83b4485aa18fd69b686b352df1558e359a9
Roger Boixader <rboixaderg@gmail.com>
2021-01-16 23:16:18 +0100

    [Feat] Add edit componentd and download field render component

---
+++ src/guillo-gmi/components/fields/editComponent.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Textarea } from '../input/textarea'
+import { Checkbox } from '../input/checkbox'
+import { FileUpload } from '../input/upload'
+import { Select } from '../input/select'
+import { Input } from '../input/input'
+import { get } from '../../lib/utils'
+export const EditComponent = React.forwardRef(
+  ({ schema, val, setValue }, ref) => {
+    if (schema?.widget === 'textarea' || schema?.widget === 'richtext') {
+      return (
+        <Textarea
+          value={val || ''}
+          className="is-small"
+          onChange={(ev) => setValue(ev)}
+          ref={ref}
+        />
+      )
+    } else if (schema?.type === 'boolean') {
+      return (
+        <Checkbox
+          value={val || ''}
+          className="is-small"
+          onChange={(ev) => setValue(ev)}
+          ref={ref}
+        />
+      )
+    } else if (schema?.widget === 'file') {
+      return (
+        <FileUpload
+          onChange={(ev) => setValue(ev)}
+          label={get(val, 'filename', null)}
+        />
+      )
+    } else if (schema?.widget === 'select') {
+      return (
+        <Select
+          value={val || ''}
+          className="is-small"
+          appendDefault
+          options={schema?.vocabulary.map((item) => {
+            return {
+              text: item,
+              value: item,
+            }
+          })}
+          onChange={(ev) => {
+            const selectValue = get(ev, 'target.value', '')
+            return setValue(selectValue)
+          }}
+        />
+      )
+    }
+    return (
+      <Input
+        value={val || ''}
+        className="is-small"
+        onChange={(ev) => setValue(ev)}
+        ref={ref}
+      />
+    )
+  }
+)
+
+EditComponent.displayName = 'EditComponent'
+export default EditComponent
