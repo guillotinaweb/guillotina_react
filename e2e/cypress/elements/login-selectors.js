fcf45b509de47b0b9b9bfbbb46d5aa6b1a806e80
Roger Boixader <rboixaderg@gmail.com>
2021-01-24 13:23:10 +0100

    [Test] Use data-test selectors in login test

---
+++ e2e/cypress/elements/login-selectors.js
@@ -0,0 +1,6 @@
+export const LOGIN_SELECTORS = {
+  form: "[data-test='formLoginTest']",
+  username: "[data-test='inputUsernameLoginTest']",
+  password: "[data-test='inputPasswordLoginTest']",
+  btnSubmit: "[data-test='btnLoginTest']",
+}
