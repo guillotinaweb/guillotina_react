775c87077134bfd9b64bc25ab126ab026ed65b8a
Roger Boixader Güell <rboixaderg@gmail.com>
2026-01-04 18:29:49 +0100

    Refactor application configuration and enhance workflow definitions

--- guillotina_example/guillotina_react_app/guillotina_react_app/__init__.py
+++ guillotina_example/guillotina_react_app/guillotina_react_app/__init__.py
@@ -1,14 +1,25 @@
 from guillotina import configure
-from guillotina_react_app.workflow import guillotina_basic_with_translations
+from guillotina_react_app.workflow import guillotina_basic_with_translations, guillotina_simple
 
 app_settings = {
+    "applications": [
+        "guillotina.contrib.catalog.pg",
+        "guillotina.contrib.workflows",
+        "guillotina.contrib.image",
+        "guillotina.contrib.swagger",
+        "guillotina.contrib.vocabularies",
+        "guillotina.contrib.dbusers",
+    ],
     "commands": {
         "populate": "guillotina_react_app.commands.populate.PopulateData",
     },
-     "workflows": {"guillotina_basic_with_translations": guillotina_basic_with_translations},
+    "workflows": {
+        "guillotina_basic_with_translations": guillotina_basic_with_translations,
+        "guillotina_simple": guillotina_simple,
+    },
     "workflows_content": {
         "guillotina_react_app.gmi.interface.IGMI": "guillotina_basic_with_translations",
-        "guillotina_react_app.gmi.interface.IGMIBehaviors": "guillotina_simple",
+        "guillotina_react_app.gmi_behaviors.interface.IGMIBehaviors": "guillotina_simple"
     },
 }
 
